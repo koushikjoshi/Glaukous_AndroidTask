0c9c2b1bef6724444c51f698ce62c3f5186139fa
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2022-09-27 23:31:26 +0530

    CardView wireframes added

--- app/src/main/java/com/koushikjoshi/glaukous_androidtask/HomeFragment.kt
+++ app/src/main/java/com/koushikjoshi/glaukous_androidtask/HomeFragment.kt
@@ -35,18 +35,11 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
